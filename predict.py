9264b291f5d4c59b1221ce8f065d7bc3663ff03d
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-21 04:46:50 +0900

    print

---
+++ predict.py
@@ -0,0 +1,82 @@
+#=======[import default libraries]=======
+import os
+import pickle
+import sys
+from datetime import datetime
+
+#=======[import libraries]=======
+from diffusers import DDIMScheduler
+from diffusers.optimization import get_cosine_schedule_with_warmup
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.transforms import Compose, Resize, Normalize, ToTensor
+from tqdm.auto import tqdm
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPImageProcessor
+
+#=======[import own libraries]=======
+from utils.loss import get_loss
+from utils.dataset import CustomDataset
+from utils.config import TrainingConfig
+from utils.check_gpu import display_gpu
+from utils.get_model import getModel
+from diffusers_lib.pipelines.controlnet.pipeline_controlnet import StableDiffusionControlNetPipeline
+from diffusers_lib.models.controlnet import ControlNetModel
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+dtype = torch.float32 if device == torch.device('cuda') else torch.float32
+
+# import config
+config = TrainingConfig()
+
+
+# set transform
+transform = Compose([
+    Resize((768, 768)),  # 768x768にリサイズ
+    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# import models
+unet = getModel("unet").to(device).to(dtype=dtype)
+controlnet = torch.load("weights/20231221_025011.ckpt").to(device).to(dtype=dtype)
+vae = getModel("vae").to(device).to(dtype=dtype)
+noise_scheduler = DDIMScheduler.from_pretrained("weights/stable-diffusion-2-1/scheduler", subfolder="scheduler")
+tokenizer = CLIPTokenizer.from_pretrained("weights/stable-diffusion-2-1/tokenizer")
+text_encoder =  CLIPTextModel.from_pretrained("weights/stable-diffusion-2-1/text_encoder").to(device).to(dtype=dtype)
+feature_extractor = CLIPImageProcessor.from_pretrained("weights/stable-diffusion-2-1/feature_extractor")
+
+# to eval mode　
+controlnet.requires_grad_(False)
+unet.requires_grad_(False)
+vae.requires_grad_(False)
+text_encoder.requires_grad_(False)
+
+# set pipline
+pipe = StableDiffusionControlNetPipeline(
+    vae=vae,
+    text_encoder=text_encoder,
+    tokenizer=tokenizer,
+    unet=unet,
+    controlnet=controlnet,
+    scheduler=noise_scheduler,
+    feature_extractor=feature_extractor,
+)
+
+#========[predict]========
+pipe.enable_attention_slicing()
+image_path="golf.jpg"
+
+# PILを使って画像を読み込む
+image = Image.open(image_path)
+
+# 画像を変換
+tensor_image = transform(image)
+
+# バッチ次元を追加
+
+tensor_image = tensor_image.unsqueeze(0)
+prompt = [""] * config.train_batch_size
+output = pipe( prompt=prompt, image=tensor_image)
+
+print(output)
