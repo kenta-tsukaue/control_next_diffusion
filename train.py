98fda1f2b06dfdd1cb1195fcdb84d0f253e8665c
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-20 18:14:19 +0900

    savemodel test

--- train.py
+++ train.py
@@ -82,8 +82,8 @@ def train_loop(
             optimizer.step()
             lr_scheduler.step()
 
-            # stepが50の倍数のときに進捗とlossを表示
-            if step % 50 == 0:
+            # stepが100の倍数のときに進捗とlossを表示
+            if step % 100 == 0:
                 print(f"epoch: {epoch}, step: {step}/{len(train_dataloader)}, loss: {loss.item()}")
 
         
@@ -99,22 +99,23 @@ def train_loop(
             )
             evaluate(config, epoch, pipeline)"""
 
-        if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+        #if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+        #if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
             # save model
-            print("save model")
-            save_dir = save_dir = "./weights"
-            # ディレクトリが存在しない場合は作成
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
+        print("save model")
+        save_dir = save_dir = "./weights"
+        # ディレクトリが存在しない場合は作成
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
-            # 現在のタイムスタンプを取得
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        # 現在のタイムスタンプを取得
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-            # ファイルパスを設定
-            save_path = os.path.join(save_dir, f"{timestamp}.ckpt")
+        # ファイルパスを設定
+        save_path = os.path.join(save_dir, f"{timestamp}.ckpt")
 
-            # モデルを保存
-            torch.save(controlnet, save_path)
+        # モデルを保存
+        torch.save(controlnet, save_path)
 
 
 
