e6b5539b321605ec43c0554840b94b508bdbb532
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-22 14:53:28 +0900

    change predict

--- predict.py
+++ predict.py
@@ -28,61 +28,67 @@ from diffusers_lib.models.controlnet import ControlNetModel
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 dtype = torch.float32 if device == torch.device('cuda') else torch.float32
 
-# import config
-config = TrainingConfig()
 
+def predict(vae, text_encoder, tokenizer, unet, controlnet, noise_scheduler, feature_extractor):
+    # set pipline
+    pipe = StableDiffusionControlNetPipeline(
+        vae=vae,
+        text_encoder=text_encoder,
+        tokenizer=tokenizer,
+        unet=unet,
+        controlnet=controlnet,
+        scheduler=noise_scheduler,
+        safety_checker=None,
+        feature_extractor=feature_extractor,
+    )
 
-# set transform
-transform = Compose([
-    ToTensor(),
-    Resize((768, 768)),  # 768x768にリサイズ
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+    # set transform
+    transform = Compose([
+        ToTensor(),
+        Resize((768, 768)),  # 768x768にリサイズ
+        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-# import models
-unet = getModel("unet").to(device).to(dtype=dtype)
-controlnet = torch.load("weights/20231221_025011.ckpt").to(device).to(dtype=dtype)
-#controlnet = ControlNetModel.from_unet(unet).to(device).to(dtype=dtype)
-vae = getModel("vae").to(device).to(dtype=dtype)
-noise_scheduler = DDIMScheduler.from_pretrained("weights/stable-diffusion-2-1/scheduler", subfolder="scheduler")
-tokenizer = CLIPTokenizer.from_pretrained("weights/stable-diffusion-2-1/tokenizer")
-text_encoder =  CLIPTextModel.from_pretrained("weights/stable-diffusion-2-1/text_encoder").to(device).to(dtype=dtype)
-feature_extractor = CLIPImageProcessor.from_pretrained("weights/stable-diffusion-2-1/feature_extractor")
+    #========[predict]========
+    pipe.enable_attention_slicing()
+    image_path="golf.jpg"
 
-# to eval mode　
-controlnet.requires_grad_(False).eval()
-unet.requires_grad_(False).eval()
-vae.requires_grad_(False).eval()
-text_encoder.requires_grad_(False).eval()
+    # PILを使って画像を読み込む
+    image = Image.open(image_path)
 
-# set pipline
-pipe = StableDiffusionControlNetPipeline(
-    vae=vae,
-    text_encoder=text_encoder,
-    tokenizer=tokenizer,
-    unet=unet,
-    controlnet=controlnet,
-    scheduler=noise_scheduler,
-    safety_checker=None,
-    feature_extractor=feature_extractor,
-)
+    # 画像を変換
+    tensor_image = transform(image)
 
-#========[predict]========
-pipe.enable_attention_slicing()
-image_path="golf.jpg"
+    # バッチ次元を追加
 
-# PILを使って画像を読み込む
-image = Image.open(image_path)
+    tensor_image = tensor_image.unsqueeze(0)
+    prompt = [""] 
+    output = pipe( prompt=prompt, image=tensor_image)
 
-# 画像を変換
-tensor_image = transform(image)
+    image_data = output[0][0]
 
-# バッチ次元を追加
+    image_data.save('output_file.png')
 
-tensor_image = tensor_image.unsqueeze(0)
-prompt = [""] * config.train_batch_size
-output = pipe( prompt=prompt, image=tensor_image)
+def main(vae, text_encoder, tokenizer, unet, controlnet, noise_scheduler, feature_extractor):
 
-image_data = output[0][0]
+    
+    # import models
+    unet = getModel("unet").to(device).to(dtype=dtype)
+    controlnet = torch.load("weights/20231221_025011.ckpt").to(device).to(dtype=dtype)
+    #controlnet = ControlNetModel.from_unet(unet).to(device).to(dtype=dtype)
+    vae = getModel("vae").to(device).to(dtype=dtype)
+    noise_scheduler = DDIMScheduler.from_pretrained("weights/stable-diffusion-2-1/scheduler", subfolder="scheduler")
+    tokenizer = CLIPTokenizer.from_pretrained("weights/stable-diffusion-2-1/tokenizer")
+    text_encoder =  CLIPTextModel.from_pretrained("weights/stable-diffusion-2-1/text_encoder").to(device).to(dtype=dtype)
+    feature_extractor = CLIPImageProcessor.from_pretrained("weights/stable-diffusion-2-1/feature_extractor")
 
-image_data.save('output_file.png')
+    # to eval mode　
+    controlnet.requires_grad_(False).eval()
+    unet.requires_grad_(False).eval()
+    vae.requires_grad_(False).eval()
+    text_encoder.requires_grad_(False).eval()
+
+    predict(vae, text_encoder, tokenizer, unet, controlnet, noise_scheduler, feature_extractor)
+    
+if __name__ == "__main__":
+    main()
